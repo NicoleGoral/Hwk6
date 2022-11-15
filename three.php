f99e79210bfc8d06d71c0e2a4c9f35377b13a4ad
Nicole Goral <78056391+NicoleGoral@users.noreply.github.com>
2022-11-15 10:08:22 -0600

    Update three.php

--- three.php
+++ three.php
@@ -1,6 +1,9 @@
 <?php require_once("header.php"); ?>
 <!DOCTYPE html>
 <html>
+  
+<h1 style="background-color:yellow; text-align:center;">Nicole's Homework 6: JavaScript Libraries</h1>
+  
 <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
 
 <body>

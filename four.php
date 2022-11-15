4c371ecc3af581a9d781b75e5d5f374a3090f4cf
Nicole Goral <78056391+NicoleGoral@users.noreply.github.com>
2022-11-15 11:18:03 -0600

    Update four.php

--- four.php
+++ four.php
@@ -7,39 +7,76 @@
 <div id="myPlot" style="width:100%;max-width:700px"></div>
 
 <script>
-var exp1 = "x";
-var exp2 = "1.5*x";
-var exp3 = "1.5*x + 7";
-// Generate values
+data = [
+  {
+    type: "scatterpolar",
+    mode: "lines",
+    r: [0, 1.5, 1.5, 0, 2.5, 2.5, 0],
+    theta: [0, 10, 25, 0, 205, 215, 0],
+    fill: "toself",
+    fillcolor: '#709BFF',
+    line: {
+      color: 'black'
+    }
+  },
+  {
+    type: "scatterpolar",
+    mode: "lines",
+    r: [0, 3.5, 3.5, 0],
+    theta: [0, 55, 75, 0],
+    fill: "toself",
+    fillcolor: '#E4FF87',
+    line: {
+      color: 'black'
+    }
+  },
+  {
+    type: "scatterpolar",
+    mode: "lines",
+    r: [0, 4.5, 4.5, 0, 4.5, 4.5, 0],
+    theta: [0, 100, 120, 0, 305, 320, 0],
+    fill: "toself",
+    fillcolor: '#FFAA70',
+    line: {
+      color: 'black'
+    }
+  },
+  {
+    type: "scatterpolar",
+    mode: "lines",
+    r: [0, 4, 4, 0],
+    theta: [0, 165, 195, 0],
+    fill: "toself",
+    fillcolor: '#FFDF70',
+    line: {
+      color: 'black'
+    }
+  },
+  {
+    type: "scatterpolar",
+    mode: "lines",
+    r: [0, 3, 3, 0],
+    theta: [0, 262.5, 277.5, 0],
+    fill: "toself",
+    fillcolor: '#B6FFB4',
+    line: {
+      color: 'black'
+    }
+  }
+]
 
-var x1Values = [];
-var x2Values = [];
-var x3Values = [];
-var y1Values = [];
-var y2Values = [];
-var y3Values = [];
-
-for (var x = 0; x <= 10; x += 1) {
-  x1Values.push(x);
-  x2Values.push(x);
-  x3Values.push(x);
-  y1Values.push(eval(exp1));
-  y2Values.push(eval(exp2));
-  y3Values.push(eval(exp3));
+layout = {
+  polar: {
+    radialaxis: {
+      visible: true,
+      range: [0, 5]
+    }
+  },
+  showlegend: false
 }
 
-// Define Data
-var data = [
-  {x: x1Values, y: y1Values, mode:"lines"},
-  {x: x2Values, y: y2Values, mode:"lines"},
-  {x: x3Values, y: y3Values, mode:"lines"}
-];
+Plotly.newPlot('myPlot', data, layout)
 
-//Define Layout
-var layout = {title: "[y=" + exp1 + "]  [y=" + exp2 + "]  [y=" + exp3 + "]"};
-
-// Display using Plotly
-Plotly.newPlot("myPlot", data, layout);
 </script>
 
 </body>

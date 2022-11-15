d2122b98298c2509382853c2c45cf2c903344874
Nicole Goral <78056391+NicoleGoral@users.noreply.github.com>
2022-11-15 10:58:07 -0600

    Update two.php

--- two.php
+++ two.php
@@ -10,41 +10,22 @@
 <div id="myPlot" style="width:100%;max-width:700px"></div>
 
 <script>
-var x = ['day 1', 'day 1', 'day 1', 'day 1', 'day 1', 'day 1',
-         'day 2', 'day 2', 'day 2', 'day 2', 'day 2', 'day 2']
-
 var trace1 = {
-  y: [0.2, 0.2, 0.6, 1.0, 0.5, 0.4, 0.2, 0.7, 0.9, 0.1, 0.5, 0.3],
-  x: x,
-  name: 'kale',
-  marker: {color: '#3D9970'},
-  type: 'box'
+  x: [1, 2, 3, 4, 4, 4, 8, 9, 10],
+  type: 'box',
+  name: 'Set 1'
 };
 
 var trace2 = {
-  y: [0.6, 0.7, 0.3, 0.6, 0.0, 0.5, 0.7, 0.9, 0.5, 0.8, 0.7, 0.2],
-  x: x,
-  name: 'radishes',
-  marker: {color: '#FF4136'},
-  type: 'box'
+  x: [2, 3, 3, 3, 3, 5, 6, 6, 7],
+  type: 'box',
+  name: 'Set 2'
 };
 
-var trace3 = {
-  y: [0.1, 0.3, 0.1, 0.9, 0.6, 0.6, 0.9, 1.0, 0.3, 0.6, 0.8, 0.5],
-  x: x,
-  name: 'carrots',
-  marker: {color: '#FF851B'},
-  type: 'box'
-};
-
-var data = [trace1, trace2, trace3];
+var data = [trace1, trace2];
 
 var layout = {
-  yaxis: {
-    title: 'normalized moisture',
-    zeroline: false
-  },
-  boxmode: 'group'
+  title: 'Horizontal Box Plot'
 };
 
 Plotly.newPlot('myDiv', data, layout);
